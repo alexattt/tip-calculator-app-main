34b95130d2b6b2bd1822c68120fb88c307dd7c59
Aleksandra Timofejeva <aleksandra1338@inbox.lv>
2021-12-27 02:54:19 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function () {
+
+  // this is how readonly input field placeholder value is changed
+  // let tipPerPerson = this.document.getElementsByName('tip-per-person');
+  // tipPerPerson[0].placeholder = "$5.25"
+
+  // let totalPerPerson = this.document.getElementsByName('total-per-person');
+  // totalPerPerson[0].placeholder = "$30.76"
+  
+})
